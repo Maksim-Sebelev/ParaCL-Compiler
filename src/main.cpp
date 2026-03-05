8b623ebccf4c3de3da568a5e1035893a8de87c0c
Maksim-Sebelev <7905111@list.ru>
2026-03-05 21:04:30 +0300

    Revert "add option - if not ouput file specifide - a.out"
    This reverts commit ed3e71bb3aa29ec9f06d81a569f205aada01ba3e.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,10 @@
+
 #warning "Using version, with stupid options parsing"
-
-#include <iostream>
-#include <exception>
-
 import compiler;
 
-int main(int argc, char* argv[]) try
+int main(int argc, char* argv[])
 {
-    if (argc == 2)
-        compiler::compile(argv[1], "a.out");
-    else
-        compiler::compile(argv[1]/* = .ast.json */, /* argv[2] = -o , */ argv[3] /* = executable */);
+    compiler::compile(argv[1]/* = .ast.json */, /* argv[2] = -o , */ argv[3] /* = executable */);
 
     return 0;
-}
-catch (std::exception const & e)
-{
-    std::cerr << "Exception cathched: " << e.what() << "\n";
-    return 1;
-}
-catch (...)
-{
-    std::cerr << "Undefined exceptions catched.\n";
-    return 1;
 }
